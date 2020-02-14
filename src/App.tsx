d0e753e59f5c7f483bd349761466d44e075db627
ossan-engineer <bodega1123@gmail.com>
2020-02-14 23:16:50 +0900

    Add effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login';
 import Header from './components/Header';
 
 const App = () => {
   const [user, setUser] = useState('');
+
+  useEffect(() => {
+    document.title = user ? `${user}'s Feed` : 'Please login';
+  }, [user]);
 
   if (!user) {
     return <Login setUser={setUser} />;
